1c5eddea54e1e4914302fcd735ad24a53cf13f9a
Alexandra Baker <baker.alexandra88@gmail.com>
2022-02-10 13:08:23 +0100

    Start express in server.js

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+import express from "express";
+
+const app = express();
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(
+        `started on http://localhost:${server.address().port}`
+    );
+});
