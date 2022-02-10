75e92ae315582133942adfbdf22e3e953c67ea4b
Alexandra Baker <baker.alexandra88@gmail.com>
2022-02-10 20:06:21 +0100

    Added empty post request and tested connection

--- quizApp.js
+++ quizApp.js
@@ -5,3 +5,7 @@ export const QuizApp = express.Router();
 QuizApp.get("/random", (req, res) => {
     res.send("Test random question");
 });
+
+QuizApp.post("/answer", (req, res) => {
+    res.end();
+})
