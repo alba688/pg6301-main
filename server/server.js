422b80f3e72182c7f7f879278377a1910aaf56d6
Alexandra Baker <baker.alexandra88@gmail.com>
2022-02-14 12:44:42 +0100

    Simple React App

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 
 const app = express();
-app.use(express.static("public"));
 
 app.get("/login", (req, res, next) => {
   res.json({ username: "Noen André Persson" });
@@ -9,6 +8,8 @@ app.get("/login", (req, res, next) => {
 app.post("/login", (req, res, next) => {
   res.sendStatus(401);
 });
+
+app.use(express.static("public"));
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
