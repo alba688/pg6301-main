ee6f66583745f23c1aa34d14b294adf62dab2d52
Alexandra Baker <baker.alexandra88@gmail.com>
2022-02-15 12:27:39 +0100

    Added middleware

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { randomQuestion } from "./questions.js";
 import bodyParser from "body-parser";
+import * as path from "path";
 
 const app = express();
-app.use(express.static("../client/dist"));
 app.use(bodyParser.json());
+
+app.use(express.static(path.resolve("../client/dist")));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
 
 app.get("/quiz", (req, res, next) => {
   const question = randomQuestion();
@@ -12,7 +21,7 @@ app.get("/quiz", (req, res, next) => {
 });
 
 app.post("/quiz", (req, res, next) => {
-  res.sendStatus(401);
+  res.sendStatus(200);
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
