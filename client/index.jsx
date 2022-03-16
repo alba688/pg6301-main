4e5cf94a104f3eb611abea6fbc5094af433860b7
Alexandra Baker <baker.alexandra88@gmail.com>
2022-03-16 20:04:21 +0100

    Setup Express and React with scripts for Heroku

---
+++ client/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+
+function FrontPage() {
+    return (
+        <div>
+            <h1>Front Page</h1>
+            <div>
+                <Link to="/login">Login</Link>
+            </div>
+            <div>
+                <Link to="/profile">Profile</Link>
+            </div>
+        </div>
+    );
+}
+
+function Application() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<FrontPage />} />
+                <Route path={"/login"} element={<h1>Login</h1>} />
+                <Route path={"/login/callback"} element={<h1>Login callback</h1>} />
+                <Route path={"/profile"} element={<h1>Profile</h1>} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
