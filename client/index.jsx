f399aeead5d997333b0d0741791440a41ad60209
Alexandra Baker <baker.alexandra88@gmail.com>
2022-03-18 15:02:08 +0100

    Client cleanup and avoid accidental capture of old chatLog

--- client/index.jsx
+++ client/index.jsx
@@ -41,31 +41,18 @@ function ChatApplication({ username }) {
         ws.onmessage = (event) => {
             console.log(event.data);
             const { author, message } = JSON.parse(event.data);
-            setChatLog([...chatLog, { author, message }]);
+            setChatLog((oldState) => [...oldState, { author, message }]);
         };
         setWs(ws);
     }, []);
 
-
-    const [chatLog, setChatLog] = useState([
-        {
-            author: "Allie",
-            message: "Hello",
-        },
-        {
-            author: "Vegard",
-            message: "Hello",
-        },
-        {
-            author: "Christian",
-            message: "Hello to you",
-        },
-    ]);
+    const [chatLog, setChatLog] = useState([]);
     const [message, setMessage] = useState("");
 
     function handleNewMessage(event) {
         event.preventDefault();
-        setChatLog([...chatLog, { author: username, message }]);
+        const chatMessage = { author: username, message};
+        ws.send(JSON.stringify(chatMessage));
         setMessage("");
     }
 
@@ -88,7 +75,7 @@ function ChatApplication({ username }) {
 }
 
 function Application() {
-    const [username, setUsername] = useState("Allie");
+    const [username, setUsername] = useState();
 
     if (!username) {
         return <Login onLogin={(username) => setUsername(username)} />;
--- server/server.js
+++ server/server.js
@@ -6,10 +6,21 @@ const app = express();
 app.use(express.static("../client/dist"));
 
 const wsServer = new WebSocketServer({ noServer: true });
+
+const sockets = [];
+
 wsServer.on("connect", (socket) => {
+    sockets.push(socket);
     setTimeout(() => {
         socket.send(JSON.stringify({ author: "Server", message: "Hello there" }));
     }, 1000);
+
+    socket.on("message", (data) => {
+        const { author, message } = JSON.parse(data);
+        for (const recipient of sockets) {
+            recipient.send(JSON.stringify({ author, message }));
+        }
+    });
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
