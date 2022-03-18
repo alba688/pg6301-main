1c9183beb7e2741e20d68d8d8ec2b37c1f02a451
Alexandra Baker <baker.alexandra88@gmail.com>
2022-03-18 14:32:43 +0100

    Logic for chat

--- client/index.jsx
+++ client/index.jsx
@@ -25,18 +25,49 @@ function Login({ onLogin }) {
     );
 }
 
-function ChatApplication({username}) {
+function ChatMessage({ chat: { author, message } }) {
+    return (
+        <div>
+            <strong>{author}: </strong>
+            {message}
+        </div>
+    );
+}
+
+function ChatApplication({ username }) {
+    const [chatLog, setChatLog] = useState([
+        {
+            author: "Allie",
+            message: "Hello",
+        },
+        {
+            author: "Vegard",
+            message: "Hello",
+        },
+        {
+            author: "Christian",
+            message: "Hello to you",
+        },
+    ]);
+    const [message, setMessage] = useState("");
+
     function handleNewMessage(event) {
         event.preventDefault();
+        setChatLog([...chatLog, { author: username, message }]);
+        setMessage("");
     }
 
     return (
         <div className={"application"}>
             <header>Chat application {username}</header>
-            <main>Here is the main content</main>
+            <main>
+                {chatLog.map((chat, index) => (
+                    <ChatMessage key={index} chat={chat} />
+                ))}
+            </main>
             <footer>
                 <form onSubmit={handleNewMessage}>
-                    <input />
+                    <input value={message} onChange={(e) => setMessage(e.target.value)} />
                     <button>Submit</button>
                 </form>
             </footer>
@@ -45,7 +76,7 @@ function ChatApplication({username}) {
 }
 
 function Application() {
-    const [username, setUsername] = useState();
+    const [username, setUsername] = useState("Allie");
 
     if (!username) {
         return <Login onLogin={(username) => setUsername(username)} />;
@@ -53,6 +84,5 @@ function Application() {
 
     return <ChatApplication username={username} />;
 }
-
 
 ReactDOM.render(<Application />, document.getElementById("app"));
